b33e0630011e4e19aa0a977512f2c2127d7f74a3
timtiph <tiphany.moine@outlook.com>
2022-11-04 12:32:23 +0100

    dark mode finalisation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
 import Content from './Components/Content/Content';
+import ThemeContextProvider from './Context/ThemeContext';
 
 function App() {
   return (
     <div className="App">
-      <Content></Content>
+      <ThemeContextProvider>
+
+      <Content>
+        
+      </Content>
+
+      </ThemeContextProvider>
     </div>
   );
 }
--- src/Components/BtnToggle/BtnToggle.js
+++ src/Components/BtnToggle/BtnToggle.js
@@ -1,11 +1,16 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './BtnToggle.css'
+import { ThemeContext } from '../../Context/ThemeContext'
 
 export default function BtnToggle() {
-    return (
 
-        <button className='btn-toggle'>
-Toggle
+    const { toggleTheme, theme } = useContext(ThemeContext)
+
+    return (
+        <button
+            onClick={toggleTheme}
+            className={theme ? "btn-toggle" : "btn-toggle dark-btn"}>
+            {theme ? "LIGHT" : "DARK"}
         </button>
     )
 }
