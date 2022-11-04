c35caffba43b37a3fb186b7aecf79460267cd915
timtiph <tiphany.moine@outlook.com>
2022-11-04 11:32:05 +0100

    creation BtnToggle

---
+++ src/Components/BtnToggle/BtnToggle.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './BtnToggle.css'
+
+export default function BtnToggle() {
+    return (
+
+        <button className='btnToggle'>
+Toggle
+        </button>
+    )
+}
